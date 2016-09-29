9693f79c3b086474c0fa4974cb7269712d5fa186
nickscaglione <nicholas.a.scaglione@gmail.com>
2016-09-29 13:30:19 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_array)
+    words_array.select do |potential_match|
+      potential_match.split("").sort == @word.split("").sort
+    end
+  end
+
+end
